41735d2b20320ad6902b6b8b065750f560a5936b
mtcf987 <mtcf987@gmail.com>
2023-10-29 13:43:48 -0700

    Functions Script

---
+++ opsch03.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script Name:               Functions Overview
+# Author:                    Malena Tomlin
+# Date of Latest Version:    10/25/2023
+# Purpose:                   The explanation of functions.
+
+# Declaration of  variables:
+
+# Declaration of functions:
+login()
+{
+last
+echo "This is the login history!"
+}
+# Main
+login
+# End
+
