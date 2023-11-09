bb1805f0c38ce4e43cf319f59b8514e9c3513c80
mtcf987 <mtcf987@gmail.com>
2023-11-09 10:38:23 -0800

    Analyzers

--- opsch13.sh
+++ opsch13.sh
@@ -6,24 +6,25 @@
 # Purpose:                  Exploring and Running Domain Analyzers
 #                             -Create a script that asks a user to type a domain, then displays information about the typed domain. Operations that must be used include: whois, dig, host, and nslookup.
 
-# Domain: zoho.com
+# Domain to test: zoho.com
+
+# Run the set of commands against the domain you want to test and place  it in a text file.
+# Function is to combine all of the commands in one spot.
+
+get_domain_info ()
+{
+    # Commands I want to run.
+    echo "This is the result of the whois command!"
+    whois $domain
+    dig $domain
+    host $domain
+    nslookup $domain
+}
 
 # Main
 
-# For whois command:
-whois zoho.com
-
-# For dig command:
-echo "DNS Lookup (dig):"
-dig zoho.com
-
-# For host command:
-echo "Host Information:"
-host zoho.com
-
-# For nslookup command:
-echo "DNS Lookup (nslookup):"
-nslookup zoho.com
+# Call Function and Redirect the output to the text file
+get_domain_info > domain_info.txt
 
 # End of script
 
