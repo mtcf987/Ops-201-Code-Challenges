a3f139b638f7db6f276b1e50d8b6a3d9b6608313
Malena Tomlin <142044465+mtcf987@users.noreply.github.com>
2023-11-12 19:36:47 -0800

    Add files via upload

---
+++ OpsCh11 - Automated Endpoint Configuration.ps1
@@ -0,0 +1,24 @@
+﻿# Malena Tomlin
+# Ops Challenge 11
+
+# Purpose: Write a Powershell script that automates the configuration of a new Windows 10 endpoint. Your script should perform the following (listed below)
+
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+# Enable ICMP
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 
+
+# Enable RDP
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+# Remove Bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+
+
